124088c604968a6d9647c3ba6db0b40c6e6c55de
sumitkumar <Sumitkumar808393@gmail.com>
2025-07-01 23:44:51 +0530

    Sum of Odd

---
+++ src/SumOfOdd.java
@@ -0,0 +1,33 @@
+//
+//
+//public class SumOfOdd
+//{
+//    public static void main(String args[])
+//    {
+//        int sum = 0;
+//        for (int i = 1; i <= 100; i++)
+//        {
+//            if (i % 2 != 0)
+//            {
+//                sum = sum + i;
+//            }
+//        }
+//        System.out.println("The Sum Of 100 Odd Numbers are:" + sum);
+//    }
+//}
+
+public class SumOfOdd {
+    public static void main(String[] args){
+
+        int sum = 0;
+
+        for(int i=1;i<=100;i++){
+            if(i%2!=0){
+                sum = sum +i;
+            }
+
+        }
+        System.out.println("Number is "+ sum);
+
+    }
+}
