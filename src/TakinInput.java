cfaf69f96b2865ce9203839def2180be6f64cd2e
sumitkumar <Sumitkumar808393@gmail.com>
2025-07-06 10:23:46 +0530

    Scanner

--- src/TakinInput.java
+++ src/TakinInput.java
@@ -4,8 +4,12 @@ import java.util.Scanner;
 public class TakinInput {
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter your Favouritte number");
         int n = sc.nextInt();
+        System.out.println("Enter your name");
+        String s = sc.next();
+        System.out.println("Your name is "+ s);
         System.out.println("Your Favourit number is " + n);
     }
 }
