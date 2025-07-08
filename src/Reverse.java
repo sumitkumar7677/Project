439d6f8d0c85da1f2e2a2b0a662f646b10c8a81d
Sumit kumar <sumitkumar808393@gmail.com>
2025-07-08 23:39:57 +0530

    Reverse

---
+++ src/Reverse.java
@@ -0,0 +1,9 @@
+public class Reverse {
+    public static void main(String [] args){
+        int a[] = {10,20,20,40,39};
+        for(int i = 0; i<a.length ; i++){
+
+            System.out.println("Revers of A is "+ a[i] + " ");
+        }
+    }
+}
