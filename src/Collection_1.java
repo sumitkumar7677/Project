fe531c70c9c8d60378e6bd57a9703fd253c5220a
Sumit kumar <sumitkumar808393@gmail.com>
2025-07-28 23:21:42 +0530

    C

---
+++ src/Collection_1.java
@@ -0,0 +1,20 @@
+public class Collection_1 {
+    public static void main(String[] args){
+        Bike MyBike = new Bike();
+        Order Orders = new Order(5);
+
+    }
+}
+class Bike{
+     Bike(){
+         System.out.println("Bike Created");
+     }
+}
+
+class Order{
+    Order(int items){
+        for(int i = 0; i <= items;i++){
+            System.out.println("My Second Bike Created");
+        }
+    }
+}
