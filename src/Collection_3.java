55caa61cd61d3a204ab3f9277bff84c6eced3b1b
Sumit kumar <sumitkumar808393@gmail.com>
2025-08-09 21:37:34 +0530

    Size of Collection

---
+++ src/Collection_3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Collection_3 {
+    public static void main(String[] args) {
+        // Using ArrayList as the collection
+        List<String> col = new ArrayList<>();
+        col.add("a");
+        col.add("b");
+
+        // Print size of collection
+        System.out.println(col.size());
+    }
+}
