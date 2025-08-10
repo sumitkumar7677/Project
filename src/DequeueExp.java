cd795cff6d6dc9b6c7c5b1a1601f88c71211905b
Sumit kumar <sumitkumar808393@gmail.com>
2025-08-11 00:01:35 +0530

    Queue

---
+++ src/DequeueExp.java
@@ -0,0 +1,15 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class DequeueExp {
+    public static void main(String [] args){
+        Deque<Integer> dq = new ArrayDeque<>();
+        dq.add(74);
+        dq.add(8);
+        dq.add(84);
+        dq.offerFirst(84);
+        dq.pollFirst();
+        System.out.println(dq);
+
+    }
+}
