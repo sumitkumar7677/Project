62ff08a0abc1a31c27a758cf83b41dd8cd5126a6
Sumit kumar <sumitkumar808393@gmail.com>
2025-08-12 00:33:51 +0530

    Exception Handling

---
+++ src/ExceptionHandling.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.FileReader;
+public class ExceptionHandling {
+    public static void main(String [] args){
+        System.out.println("My name is Sumit");
+        try
+        {
+            System.out.println(4/0);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        System.out.println("Exception Cross");
+    }
+}
