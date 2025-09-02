91c1ef73c9295135e529aad55e993d4fab6be068
Sumit kumar <sumitkumar808393@gmail.com>
2025-09-02 17:31:04 +0530

    Predicate

--- src/PredicateExp.java
+++ src/PredicateExp.java
@@ -4,10 +4,10 @@ import java.util.function.*;
 public class PredicateExp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter The number to Check : ");
-        int a = sc.nextInt();
+        System.out.print("Enter The Stirngg to Check : ");
+        String a = sc.nextLine();
 
-        Predicate<Integer> P1 = i->(a%2==0);
+        Predicate<String> P1 = i->(a.length()>5);
         System.out.println(P1.test(a));
 
     }
