d5243006cf5e9ff4943c1800b3b5e2f34cf72a41
Sumit kumar <sumitkumar808393@gmail.com>
2025-09-04 10:35:22 +0530

    Predicate

--- src/PredicateExp.java
+++ src/PredicateExp.java
@@ -4,7 +4,7 @@ import java.util.function.*;
 public class PredicateExp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter The Stirngg to Check : ");
+        System.out.print("Enter The Stirng to Check : ");
         String a = sc.nextLine();
 
         Predicate<String> P1 = i->(a.length()>5);
